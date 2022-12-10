67aa9fa7e4400b1acfb73b746c5159c437658c2e
Andre <aspbarruda@gmail.com>
2022-12-10 09:04:05 -0300

    Added features:     * Possibility to play new game or exit completely     * Code now show guess (with colors)     * Refactored code that receives guess input into initialize

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,6 @@
-class String
+class Integer
   def colorize(color_code)
-    puts "\e[#{color_code}m#{self}\e[0m"
+    print "\e[#{color_code}m #{self} \e[0m"
   end
 
   def red
@@ -42,8 +42,15 @@ class Game
 
   attr_reader :code
 
-  def initialize(code)
-    @code = code
+  def initialize()
+    print "Enter your guess of four numbers, from 1 to 6: "
+    @code = gets.chomp.gsub(/\s/, "")
+    until (@code.length == 4 && @code.scan(/[\D7890]/).empty?)
+      print "Wrong input, enter your guess of four numbers, from 1 to 6: "
+      @code = gets.chomp.gsub(/\s/, "")
+    end
+    puts "\n"
+    @code = @code.scan(/\d/).map(&:to_i)
   end
 
   def self.generate_code
@@ -51,17 +58,18 @@ class Game
   end
   
   def compare()
-    @@temp_answer = @@answer
+    @@temp_answer = []
+    @@answer.each {|value| @@temp_answer.push(value)}
     look_black_pins()
     look_white_pins()
     @@temp_answer = @@temp_answer - @@temp_answer.join.scan(/\d/).map(&:to_i)
-    p @@temp_answer.sort.reverse
+    @@temp_answer.sort.reverse
   end
 
   def look_black_pins()
     @@temp_answer.each_index { |i|
       if @@temp_answer[i] == @code[i]
-        @@temp_answer[i] = "\u2981" 
+        @@temp_answer[i] = "\u25cf" 
         @code[i] = nil
       end
     }
@@ -71,22 +79,66 @@ class Game
     @code.each_index { |i| 
       @@temp_answer.each_index { |j|
         if @@temp_answer[j] == @code[i] 
-          @@temp_answer[j] = "\u25e6" 
+          @@temp_answer[j] = "\u25cb" 
           @code[i] = nil
         end
       }
     }
   end
+
+  def print_colors
+    print "Your guess was: "
+    @code.each { |value|
+      case value
+      when 1
+        print value.red
+      when 2
+        print value.green
+      when 3
+        print value.yellow
+      when 4
+        print value.orange
+      when 5
+        print value.purple
+      when 6
+        print value.cyan
+      end
+      print " "
+    }
+    puts "\n\n"
+  end
+
+  def print_result(result)
+    print "Your guess was this close: "
+    result.each{ |v| print "#{v} "}
+    puts "\n\n"
+  end
 end
 
 while(true)
   Game.generate_code
-  print "Enter your guess of four numbers, from 1 to 6: "
-  guess = gets.chomp.gsub(/\s/, "")
-  until (guess.length == 4 && guess.scan(/[\D7890]/).empty?)
-    print "Wrong input, enter your guess of four numbers, from 1 to 6: "
-    guess = gets.chomp.gsub(/\s/, "")
+  max_guesses = 10
+  total_guesses = 0
+  win = false
+
+  while (total_guesses < max_guesses)
+    guess = Game.new
+    guess.print_colors
+    result = guess.compare
+    guess.print_result(result)
+    if result == Array.new(4, "\u25cf")
+      win = true
+      break
+    end
+    total_guesses += 1
   end
-  code = Game.new(guess.scan(/\d/).map(&:to_i))
-  code.compare
+
+  if win
+    puts "You win!! Wanna play again? Press 'Y' for yes or any other key for no:"
+  else
+    puts "You lost... Wanna play again? Press 'Y' for yes or any other key for no:"
+  end
+
+  selection = gets.chomp.upcase
+  break unless selection == "Y"
 end
